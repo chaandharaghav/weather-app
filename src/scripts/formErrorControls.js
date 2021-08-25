c30936880427970dc542f8aa9d833708c656b130
Chaandha Raghav <chaandharaghav.cb19@bitsathy.ac.in>
2021-08-25 14:15:33 +0530

    Add animation to error and fix bug where more than one alertbox was shown

--- src/scripts/formErrorControls.js
+++ src/scripts/formErrorControls.js
@@ -1,17 +1,18 @@
+function removeAlert() {
+  const alertDiv = document.querySelector('#alert-div');
+  if (alertDiv !== null) {
+    alertDiv.remove();
+  }
+}
+
 function alertUser(err) {
+  removeAlert();
   const alertDiv = document.createElement('div');
   alertDiv.innerText = err;
   alertDiv.id = 'alert-div';
 
   const formDiv = document.querySelector('#form-div');
   formDiv.append(alertDiv);
-}
-
-function removeAlert() {
-  const alertDiv = document.querySelector('#alert-div');
-  if (alertDiv !== null) {
-    alertDiv.remove();
-  }
 }
 
 export { alertUser, removeAlert };
